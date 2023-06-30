f339dbdc08c84ca5ab404c3f913741be6c4187fc
mk2429 <mayank.20429@knit.ac.in>
2023-06-30 16:29:20 +0530

    upd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
 import { Home } from './Components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { UseAuth } from './UseAuth';
 import { Footer } from "./Components/Footer";
 
 function App() {
-  const isLogin = UseAuth();
 
   return (
     <div className="App">
     
-      {isLogin ? <Home/> : <p> "..login First"</p>}
+       <Home/>
       <Footer/>
 
     </div>
